2dee4d224ce1bb6b497720f1a8ac18c368fd6bd1
eddyangang <eddyyang@berkeley.edu>
2020-09-04 04:04:02 -0700

    added static file

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("portfolio/build"));
+}
 app.use(routes);
 app.use(cors)
 
