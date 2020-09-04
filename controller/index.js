82ebbab46382535f02afec8f0de8ecadf9fe546e
eddyangang <eddyyang@berkeley.edu>
2020-09-04 04:20:41 -0700

    changed date to desc

--- controller/index.js
+++ controller/index.js
@@ -3,7 +3,7 @@ const db = require("../models");
 module.exports = {
 
     findAll: function (req, res) {
-        db.Projects.find({})
+        db.Projects.find({}).sort("-date")
             .then(data => res.json(data))
             .catch(err => res.status(422).json(err));
     }
