6b3ddb60b91491c8fa62f71a4b71316006f29472
eddyangang <eddyyang@berkeley.edu>
2020-10-04 18:42:34 -0700

    added 2 new projects

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 const projects = require("./projects.json")
-// This file empties the Posts collection and inserts the books below
+// This file empties the Posts collection and inserts the project below
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio_projects");
 
@@ -27,3 +27,27 @@ db.Projects.remove({})
     "date": new Date()
   }
 db.Projects.create(newProject)
+
+const newProject1 = {
+  "title": "Diabetes Tester",
+  "summary": "A web application using Python Flask backend and Streamlit frontend. This application uses machine learning and to predict the likelyhood of someone having diabetes. Additional information is also presented, like the data used to train the model, and using the charts provided correlations can be made between diabetes and biological characteristics (such as more pregnancies correlate with a higher chance of developing/having diabetes).",
+  "image_path": "./img/diabetester.gif",
+  "image_icon": "./img/cd-icon-movie.svg",
+  "github_url" : "https://github.com/eddyangang/ML_Diabetes",
+  "deployed_url" : "https://diabetester.herokuapp.com/",
+  "date": new Date()
+}
+
+db.Projects.create(newProject1)
+
+const newProject2 = {
+  "title": "Easy Reads",
+  "summary": "A fullstack web application using Python Flask for backend and React frontend. This application uses Natural Learning toolkit and newspaper (python exclusive platforms) to summarize news articles. By simply entering the URL of the news article, we can extract key words from the original article to produce a concise summary of the article. It is not perfect, but it exciting to try.",
+  "image_path": "./img/article.gif",
+  "image_icon": "./img/cd-icon-movie.svg",
+  "github_url" : "https://github.com/eddyangang/article_summary",
+  "deployed_url" : "https://easyreading.herokuapp.com/",
+  "date": new Date()
+}
+
+db.Projects.create(newProject2)
